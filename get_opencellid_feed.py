7d4f336b9cc31cb41a0e697767ddd99a825d7ec9
ashmastaflash <ash.d.wilson@gmail.com>
2017-07-02 12:13:29 -0700

    chg: usr: Switching to actual OpenCellID feed. !minor

--- get_opencellid_feed.py
+++ get_opencellid_feed.py
@@ -3,5 +3,5 @@ import opencellid
 import os
 
 api_key = os.getenv("OCID_KEY")
-ocid_obj = opencellid.OpenCellIdFeed("./")
+ocid_obj = opencellid.OpenCellIdFeed("./", api_key)
 ocid_obj.update_feed()
